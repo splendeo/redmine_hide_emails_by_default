71b692e7d743b46cde9b1c586395666ebb80654d
splendeo <support@splendeo.es>
2009-11-27 18:53:53 +0100

    added migration and readme

---
+++ db/migrate/001_hide_emails_by_default.rb
@@ -0,0 +1,9 @@
+class HideEmailsByDefault < ActiveRecord::Migration
+  def self.up
+    change_column :user_preferences, :hide_mail, :boolean, :default => true
+  end
+
+  def self.down
+    change_column :user_preferences, :hide_mail, :boolean, :default => false
+  end
+end
---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_hide_emails_by_default do
+  name 'Redmine Hide Emails By Default plugin'
+  author 'Enrique García Cota & Francisco Juan Prieto'
+  description 'This is a simple plugin that makes the [hide email to others] preference true by default'
+  version '0.0.1'
+end
